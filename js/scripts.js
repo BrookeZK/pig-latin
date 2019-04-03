70533e6e00cca3c781e34af44cb30c07be6d519f
Alex Martinez and Brooke Kullberg <brooke@>
2019-04-03 14:48:29 -0700

    add function to check if user input is a vowel and a number. Also a function to replace vowel with dash

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,51 @@
 
 var vowels = ["a", "e", "i", "o", "u", "y"]
 
-var translator = function(userInput){
-  if (typeof userInput === "number") {
-    return false
+var isNumber = function(userInputArray){
+  if (userInputArray >=0 || userInputArray <=0) {
+    return false;
   }
 }
+
+function isVowel (character) {
+  if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+  return true;
+  } else {
+  return false;
+  }
+}
+
+console.log(isVowel("o"));
+
+function replaceVowelsWithDash(character) {
+  var finalString = '';
+  for (var i = 0; i <= character.length - 1; i++) {
+    if (isVowel(character.charAt(i))) {
+      finalString += "-";
+    } else {
+      finalString += character.charAt(i);
+    }
+  }
+	return finalString;
+}
+console.log(replaceVowelsWithDash("o"));
 
 //User Interface Logic
 $(document).ready(function(){
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
+
     var userInput = $("#userInput").val();
-    var result = translator(userInput);
+    var userInputSplitString = userInput.split(" ");
+    console.log(userInput);
+    console.log(userInputSplitString);
+    var userInputArray = [];
+    userInputArray.push(userInputSplitString);
+    console.log(userInputArray);
+
+    var result = replaceVowelsWithDash(userInputArray);
+
+    // var result = translator(userInputArray);
     $("#answerArea").show();
     $("#translatedString").text(result);
   });
